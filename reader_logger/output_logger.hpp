632785f8e7051cf341c2049008d4c72b8ee85ea8
Joaquin Gatica <surrenderpls2001@gmail.com>
2025-06-02 19:19:11 -0400

    add time ejecution trayectory metrics

--- reader_logger/output_logger.hpp
+++ reader_logger/output_logger.hpp
@@ -2,42 +2,140 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <chrono> // Agrega esta línea
 
 void createFileOutput(std::string filePath, std::vector<RuntimeInfo>& data, size_t n, int entropySize) {
     for (int i = 0; i < data.size(); i++) {
         std::string filePathOutput = filePath + "_Run_" + std::to_string(i + 1) + "/";
 
         std::cerr << "Writing to Run " + std::to_string(i + 1) << std::endl;
+
+        auto start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "Fevals", data[i].getFevals());
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "Fevals: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "FevalsBest", data[i].getFevalsBest());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "FevalsBest: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].moveDomainFevals();
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "moveDomainFevals: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].getfBest();
-        data[i].calculateIntensify(); 
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "getfBest: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
+        data[i].calculateIntensify();
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "calculateIntensify: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "SumativeIntensify", data[i].getSumativeIntensify());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "SumativeIntensify: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "AccumulatedIntensify", data[i].getAccumulatedIntensify());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "AccumulatedIntensify: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "EntropyWithSphere", data[i].getEntropyWithSphere());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "EntropyWithSphere: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "SpheresAreas", data[i].getSpheresAreasForIteration(n));
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "SpheresAreas: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "ConvRate", data[i].getConvRate());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "ConvRate: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "ConvRateOpt", data[i].getConvRateOpt());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "ConvRateOpt: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "Improvement", data[i].getImprovement());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "Improvement: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "ErrorRate", data[i].getErrorRate());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "ErrorRate: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "GeomRate", data[i].getGeomRate());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "GeomRate: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "VectorDiversity", data[i].getVectorDiversity(5));
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "VectorDiversity: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "DistanceHammingfromZero", data[i].getDistanceHammingFromZero());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "DistanceHammingfromZero: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "DistanceHamming", data[i].getDistanceHamming());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "DistanceHamming: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "DistanceHammingFromLocalBest", data[i].getDistanceHammingFromLocalBest());
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "DistanceHammingFromLocalBest: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].createXMatrixPerEvaluation();
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "createXMatrixPerEvaluation: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].createDissimilarityMatrix();
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "createDissimilarityMatrix: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].MDS3();
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "MDS3: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         data[i].MDSSearchSpaceDivisionCustomSectors(entropySize);
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "MDSSearchSpaceDivisionCustomSectors: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
+        start = std::chrono::high_resolution_clock::now();
         output_file_vector(filePathOutput, "EntropyDiversity", data[i].entropyDiversityCustomAreas(entropySize));
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "EntropyDiversity: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
     }
 }
 
 void outputMetrics(std::string path, int n, int entropySize) {
     std::cerr << "Reading logger data from " << path << std::endl;
     std::string fileName = path.substr(0, path.find_first_of("/")) + "-" + path.substr(path.find_last_of("/") + 1, path.find_last_of(".") - path.find_last_of("/") - 1);
+
+    auto start = std::chrono::high_resolution_clock::now();
     std::vector<RuntimeInfo> data = readLogger(path);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "readLogger: " << std::chrono::duration<double>(end - start).count() << " s" << std::endl;
+
     std::cerr << "Creating file output with base name " << fileName << std::endl;
     createFileOutput(fileName, data, n, entropySize);
 }
